dc710ddb6d825cc0ade616f7593d1c11e6f331fa
BeccaDev <130665887+RebecadeMouraMendes@users.noreply.github.com>
2024-03-10 17:33:16 -0300

    Add files via upload

---
+++ TechExample/Estudoprova.java
@@ -0,0 +1,236 @@
+package estudoprova;
+import javax.swing.*;
+
+public class Estudoprova {
+    public static void main(String[] args) {
+    String a;
+    JOptionPane.showMessageDialog(null,"*Coloque as respostas em minúsculo, exceto em nomes próprios. Quando há períodos coloque hífem*"); 
+    JOptionPane.showMessageDialog(null,"*Trovadorismo*"); 
+    a = (JOptionPane.showInputDialog("Quem marcou o início do trovadorismo em Portugal?"));
+    if (a.equals("Paio Soares de Taveirós")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+    a = (JOptionPane.showInputDialog("Qual é a primeira cantiga? (Digite em minúsculo)"));
+    if (a.equals("cantiga da ribeirinha")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+    a = (JOptionPane.showInputDialog("Qual á a cantiga satirica que faz uma critíca direta e usa uma abordagem chula?"));
+    if (a.equals("maldizer")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+    a = (JOptionPane.showInputDialog("Digite o tipo de cantiga liríca que retrata um amor não correspondido e Eu liríco masculino."));
+    if (a.equals("amor")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+    a = (JOptionPane.showInputDialog("Qual era o idioma em que essas cantigas eram escritas?"));
+    if (a.equals("português galego")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+    a = (JOptionPane.showInputDialog("O cancioneiro de Ajuda ficava na Biblioteca de Lisboa, quantas cantigas esse cancioneiro possuia?"));
+    if (a.equals("310")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+    a = (JOptionPane.showInputDialog("Em que data o trovadorismo se iníciou? Se houver variações coloque o sinal de barra"));
+    if (a.equals("1189/1198")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+    a = (JOptionPane.showInputDialog("Em que data o trovadorismo terminou?"));
+    if (a.equals("1418")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+    a = (JOptionPane.showInputDialog("Qual cancioneiro possuía 1647 cantigas?"));
+    if (a.equals("biblioteca nacional de Lisboa")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+    a = (JOptionPane.showInputDialog("O cancioneiro do vaticano tinha quantas cantigas?"));
+    if (a.equals("1205")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+    a = (JOptionPane.showInputDialog("Qual era o nome dado ao autor das cantigas?"));
+    if (a.equals("trovador")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+    a = (JOptionPane.showInputDialog("Qual era o nome dado a quem recitava e tocava as cantigas?"));
+    if (a.equals("menestrel")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+    a = (JOptionPane.showInputDialog("Qual era o nome dado a quem declamava as cantigas?"));
+    if (a.equals("jogral")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+    a = (JOptionPane.showInputDialog("Escreva verdadeiro ou falso: As cantigas de escárnio eram diretas que exploravam palavras e construções ambíguas."));
+    if (a.equals("falso")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+    a = (JOptionPane.showInputDialog("Qual foi o país de origem?"));
+    if (a.equals("França")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+    
+        JOptionPane.showMessageDialog(null,"*Humanismo*"); 
+       
+    a = (JOptionPane.showInputDialog("Quantas sílabas poéticas tem na redondilha *maior*? Escreva por extenso"));
+    if (a.equals("sete")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+     a = (JOptionPane.showInputDialog("Quantas sílabas poéticas tem na redondilha *menor*? Escreva por extenso"));
+    if (a.equals("cinco")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+     a = (JOptionPane.showInputDialog("Peças religiosas com objetivo de divertir, moralizar ou de difundir é considerado auto ou farsa?"));
+    if (a.equals("auto")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+     a = (JOptionPane.showInputDialog("O que foi criado no humanismo, que possuía ausência da música e era feita para o entretenimento da nobreza?"));
+    if (a.equals("poesias palacianas")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+     a = (JOptionPane.showInputDialog("Quem foi o autor do Auto da Barca do Inferno?"));
+    if (a.equals("Gil Vicente")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+     a = (JOptionPane.showInputDialog("O cientificismo e racionalismo é pertencente apenas ao movimento do humanismo? Escreva verdadeiro ou falso"));
+    if (a.equals("falso")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+     a = (JOptionPane.showInputDialog("O período é considerado a transição entre o trovadorismo (idade média) e classicismo (renascimento), quais são as datas que marcam este período:?"));
+    if (a.equals("1434-1527")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+    
+    JOptionPane.showMessageDialog(null,"*Classicismo*"); 
+    
+    a = (JOptionPane.showInputDialog("Quais são as datas que marcam este período:?"));
+    if (a.equals("1537-1580")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+     a = (JOptionPane.showInputDialog("As 2 primeiras estrofes tem quantos versos? Escreva por extenso"));
+    if (a.equals("quatro")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+     a = (JOptionPane.showInputDialog("O restante das estrofes tem quantos versos? Escreva por extenso"));
+    if (a.equals("três")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+     a = (JOptionPane.showInputDialog("O classicismo tem um autor em destaque, principalmente para a história do Brasil. Quem é ele?"));
+    if (a.equals("Luís de Camões")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+     a = (JOptionPane.showInputDialog("Luís de Camões escreveu Os lusíadas que tem quantas rimas?"));
+    if (a.equals("8816")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+     a = (JOptionPane.showInputDialog("Quantos cantos Os lusíadas tem?"));
+    if (a.equals("10")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+     a = (JOptionPane.showInputDialog("Os sonetos eram criados com quantas sílabas poéticas em cada verso?"));
+    if (a.equals("10")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+      }
+     a = (JOptionPane.showInputDialog("Quem foi que iniciou o classicismo em Portugal?"));
+    if (a.equals("Sá de Miranda")){
+    JOptionPane.showMessageDialog(null,"Correto");  
+    } else
+      {
+          JOptionPane.showMessageDialog(null,"Errado :("); 
+    JOptionPane.showMessageDialog(null,"O Trovadorismo é marcado pelo teocentismo e feudalismo."); 
+         JOptionPane.showMessageDialog(null," O humanismo é a transição do trovadorismo para o classicismo, no qual a igreja começou a perder o poder e o antropocentrismo ficou em destaque.");
+            JOptionPane.showMessageDialog(null,"O classicismo é marcado pelo antopocentrismo e volta dos valores greco-romanos."); 
+    JOptionPane.showMessageDialog(null,"A separação de sílabas poéticas é feita a parir da localização da sílaba tônica da última palavra"); 
+    JOptionPane.showMessageDialog(null,"Após isso é feita normalmente a separação com exceto de palavras que terminam e outra começa com  vogal."); 
+    JOptionPane.showMessageDialog(null,"Neste caso será preciso deixar para último pois veremos por a contagem de sílabas (total = 10) se é necessário juntar ou separar as palavras."); 
+      }
+    }
+}
+
+
+
+
